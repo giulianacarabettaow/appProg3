ac5ebd17fdf69a9d5ffe3a444e9e1ff1dc43cdeb
giulianacarabettaow <gcarabettaowens@udesa.edu.ar>
2023-10-30 11:56:55 -0300

    ..

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAoNoGCscjo5vzNlqSDtkHNGRSh3r9Io9s",
+    authDomain: "appprog3-a2b17.firebaseapp.com",
+    projectId: "appprog3-a2b17",
+    storageBucket: "appprog3-a2b17.appspot.com",
+    messagingSenderId: "57416350085",
+    appId: "1:57416350085:web:3c609df1cbb34a9fcc6cec"
+  };
+  
