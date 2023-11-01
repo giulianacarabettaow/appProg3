b265268ed3f5bfe4cd0b056bc2ed5f388ec973ce
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-11-01 09:11:55 -0300

    register...

---
+++ src/screens/Login/Register/Register.js
@@ -0,0 +1,5 @@
+import react, { Component } from 'react';
+import { auth } from '../../firebase/config';
+import { TextInput, TouchableOpacity, View, Text, Stylesheet} from 'react-native';
+
+class Register 
