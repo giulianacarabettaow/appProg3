af227e46264377582b4038df2b8e456025ee2d1c
IgnacioTettamanti <itettamanti@udesa.edu.ar>
2023-11-05 14:27:48 -0300

    creando la navegacion inicial con tab navigator

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Login from './src/screens/Login/Login';
 import Register from './src/screens/Register/Register'
+
+const Tab = createBottomTabNavigator
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <Register/>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+          <Tab.Screen name='Register' component={Register} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -26,15 +26,65 @@ class Login extends Component{
 
     render(){
         return(
-                
-            <view>
-                
+            <view style={styles.formContainer}>
+            <Text>Login</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={(text)=>this.setState({email: text})}
+                placeholder='email'
+                keyboardType='email-address'
+                value={this.state.email}
+                />
+            <TextInput
+                style={styles.input}
+                onChangeText={(text)=>this.setState({password: text})}
+                placeholder='password'
+                keyboardType='default'
+                secureTextEntry={true}
+                value={this.state.password}
+            />
+            <TouchableOpacity style={styles.button} onPress={()=>this.login(this.state.email, this.state.password)}>
+                <Text style={styles.textButton}>Ingresar</Text>    
+            </TouchableOpacity>
+            <TouchableOpacity onPress={ () => this.props.navigation.navigate('Registro')}>
+               <Text>No tengo cuenta. Registrarme.</Text>
+            </TouchableOpacity>
+
             </view>
-            
         )
     
     }
-
 }
+
+const styles = StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:10,
+        marginTop: 20,
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'blue',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
 
 export default Login
